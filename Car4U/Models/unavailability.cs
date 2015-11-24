573067ecc17144ed15700a8bf30d62480be286ca
ajrgoncalves <ajrg@ua.pt>
2015-11-24 21:09:24 +0000

    unavailability

---
+++ Car4U/Models/unavailability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car4U.Models
+{
+    public class unavailability
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Observation { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string IdCar { get; set; }
+    }
+}
