8f4eb39a3e375f63e19343731658968c4f90452c
ajrgoncalves <ajrg@ua.pt>
2015-11-24 21:19:06 +0000

    Moment

---
+++ Car4U/Models/Moment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car4U.Models
+{
+    public class Moment
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Observation { get; set; }
+        public string IdReservation { get; set; }
+        public string IdMomentType { get; set; }
+    }
+}
